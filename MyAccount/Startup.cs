e5c0e5bf7f218a4007d3b3e8acea9a0601ae076a
Suicidez <32395669+Suicidez@users.noreply.github.com>
2017-09-29 23:00:06 +0700

    Add First File For Project
    Commit File New

---
+++ MyAccount/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyAccount.Startup))]
+namespace MyAccount
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
